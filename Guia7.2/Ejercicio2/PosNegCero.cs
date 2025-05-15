5b71af5ea1b08c77a28803acfeb3fec672360653
LuchoDB <lucianodiazbertozzi@gmail.com>
2025-05-15 20:06:18 -0300

    Ejercicio2 - PosNegCero

---
+++ Guia7.2/Ejercicio2/PosNegCero.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero < 0)
+            {
+                Console.WriteLine("El numero es negativo");
+            }
+            else if (numero == 0)
+            {
+                Console.WriteLine("El numero es cero");
+            }
+            else
+            {
+                Console.WriteLine("El numero es positivo");
+            }
+        }
+    }
+}
