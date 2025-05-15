a539b33a329306a930f05ebb95e8fa2b688539e7
LuchoDB <lucianodiazbertozzi@gmail.com>
2025-05-15 19:40:48 -0300

    Ejercicio1 - Condicion

---
+++ Ejercicio1/Guia7.2/Condicion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int condicion;
+            int n1, n2, n3;
+            float prom;
+
+            Console.WriteLine("Ingrese la nota minima para promocionar: ");
+            condicion = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la nota del primer examen: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la nota del segundo examen: ");
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la nota del tercer examen: ");
+            n3 = int.Parse(Console.ReadLine());
+
+            prom = (n1 + n2 + n3) / 3;
+
+            if (prom >= condicion)
+            {
+                Console.WriteLine("El alumno promociona");
+            }
+            else
+            {
+                Console.WriteLine("El alumno rinde final");
+            }
+        }
+    }
+}
