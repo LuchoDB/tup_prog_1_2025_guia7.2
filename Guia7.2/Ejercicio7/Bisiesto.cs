46bf47c5bb7bb558b1537a2e3008b495a9b2eb66
LuchoDB <lucianodiazbertozzi@gmail.com>
2025-05-16 16:53:44 -0300

    Ejercicio7 - Bisiesto

---
+++ Guia7.2/Ejercicio7/Bisiesto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un año");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            if (a%4==0 && a % 100 == 0 && a%400==0)
+            {
+                Console.WriteLine("El año es bisiesto");
+            }
+            else
+            {
+                Console.WriteLine("El año no es bisiesto");
+            }
+        }
+    }
+}
